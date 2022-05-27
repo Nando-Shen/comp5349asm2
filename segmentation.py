a2c82a7c1c94c22aaf7590f59d90cfe9493d4631
shenjiaming <shenjiaming@rd.netease.com>
2022-05-27 18:34:26 +1000

    broadcast

--- segmentation.py
+++ segmentation.py
@@ -7,7 +7,7 @@ import argparse
 # init the spark session
 spark = SparkSession.builder.appName("COMP5349 A2").getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
-spark.conf.set("spark.default.parallelism", 40)
+spark.conf.set("spark.default.parallelism", 80)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--output", help="the output path", default='output.json')
